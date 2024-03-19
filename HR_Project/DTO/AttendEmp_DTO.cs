a5a6810902e23d8853dda7caeb26100305111d55
Girgisnasim <gergesnaseem53@gmail.com>
2024-03-19 22:55:21 +0200

    save

---
+++ HR_Project/DTO/AttendEmp_DTO.cs
@@ -0,0 +1,11 @@
+﻿namespace HR_Project.DTO
+{
+    public class AttendEmp_DTO
+    {
+        public string DepartmentName { get; set; }
+        public string EmpName { get; set; }
+        public TimeSpan AttendTime  { get; set; }
+        public TimeSpan LeaveTime { get; set; }
+        public DateTime AttendDate { get; set; }
+    }
+}
